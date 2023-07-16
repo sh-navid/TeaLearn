c0d82ac3697a195e200f0e7607cfbcc569d3e8d5
sh-navid <shamshirsaz.navid@live.com>
2023-07-16 09:49:54 +0330

    Add examples

---
+++ kotlin/concepts/datatypes.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var b:Boolean = true
+    println(b)
+
+    var c:Char = 'R'
+    println(c)
+
+    var s:String = "Hello" // cannot use '
+    println(s)
+}
---
+++ kotlin/concepts/variables.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val x=12
+    // x=13 // error: val cannot be reassigned 
+    println(x)
+
+
+
+    var y=12
+    y=13
+    println(y)
+}
