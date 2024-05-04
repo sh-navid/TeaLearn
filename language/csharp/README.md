c59a0bec8616a25c26283a11ed25a9305a7ca14e
sh-navid <shamshirsaz.navid@live.com>
2024-05-04 11:34:45 +0330

    [Feature] include basics

--- language/csharp/README.md
+++ language/csharp/README.md
@@ -2,3 +2,48 @@
 - My OS for test is ubuntu
 
 ## Setup
+- `dotnet new [web|console|...]`
+
+## Execute
+- `dotnet run`
+
+## Basics
+- import
+    - `using Something`
+- Print
+    - `System.Console.WriteLine()`
+- Comment
+    - `//` and `/**/` 
+- Variable
+    - `int thisIsMyVariable=12;`
+
+## DataTypes
+- Primitives
+    - `int`
+        - `[-2147483648, 2147483647]`
+    - `double`
+    - `bool`
+    - `char`
+    - `string`
+    - `float`
+    - `long`
+        - `[-9223372036854775808, 9223372036854775807]`
+
+## TypeCast
+- Implicit
+- Explicit
+    - `(int) 12.4F`
+
+## TypeConversion
+- `System.Convert.ToInt16()`
+- `System.Convert.ToInt32()`
+- `System.Convert.ToInt64()`
+
+## Constant
+- `const [int|bool|...]`
+
+## Inputs
+- `ReadLine()`
+- `WriteLine()`
+
+## Operators
--- language/csharp/example/minimal/Program.cs
+++ language/csharp/example/minimal/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Variable, DataType
+int i=13;
+double d=12.9000000001;
+bool b=true;
+char c='a';
+string s="Test String";
+float f=12.9000000001F;
+long l=1234567890000001;
+
+// Print
+System.Console.WriteLine(i);
+System.Console.WriteLine(s);
+System.Console.WriteLine(c);
+System.Console.WriteLine(d);
+System.Console.WriteLine(f);
+System.Console.WriteLine(b);
+System.Console.WriteLine(l);
+
+// TypeCast
+int castValue=(int)d;
+System.Console.WriteLine(castValue);
+
+// TypeConvert
+System.Console.WriteLine(System.Convert.ToInt16(castValue));
+System.Console.WriteLine(System.Convert.ToInt32(castValue));
+System.Console.WriteLine(System.Convert.ToInt64(castValue));
+
+// Inputs
+System.Console.WriteLine("Write Something:");
+string inputValue= System.Console.ReadLine();
+System.Console.WriteLine(">"+inputValue);
+
+System.Console.WriteLine("Enter a number:");
+System.Console.WriteLine(">"+(Convert.ToInt32(System.Console.ReadLine())+1));
